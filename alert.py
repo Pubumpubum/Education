6227df82547aa66fee0c8a98c008e82f11347848
Pubumpubum <pubumpubum@gmail.com>
2019-10-09 15:47:19 +0300

    2

---
+++ alert.py
@@ -0,0 +1,25 @@
+import math
+from selenium import webdriver
+import time 
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+link = "http://suninjuly.github.io/alert_accept.html"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+    button1 = browser.find_element_by_class_name("btn-primary").click()
+    confirm = browser.switch_to.alert 
+    confirm.accept()
+    x = browser.find_element_by_id("input_value").text
+    y = calc(x)
+    input_answer = browser.find_element_by_id("answer").send_keys(y)
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+
+finally:
+    time.sleep(20)
+    browser.quit()
+
