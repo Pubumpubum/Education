8b48550dd90e86c1fe74d74ef79f67b4da4a3530
pubumpubum <pubumpubum@gmail.com>
2019-10-18 16:05:08 +0300

    config

--- trash.py
+++ trash.py
@@ -1,18 +1,6 @@
-import pytest
-from selenium import webdriver
-import time
-import math
+link = "http://selenium1py.pythonanywhere.com/"
 
-answer = str(math.log(int(time.time())))
-link = "https://stepik.org/lesson/236899/step/1"
 
-try:
-    browser = webdriver.Chrome()
+def test_guest_should_see_login_link(browser):
     browser.get(link)
-    browser.implicitly_wait(5)
-    browser.find_element_by_class_name("textarea").send_keys(answer)
-    browser.find_element_by_class_name("submit-submission").click()
-
-finally:
-    time.sleep(50)
-    browser.quit()
+    browser.find_element_by_css_selector("#login_link")
